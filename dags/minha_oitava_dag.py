44b972fe14243b89cf782d78fef065269e7d11bf
grwtorves <igor.torves@grwt.com.br>
2026-06-21 11:09:02 -0300

    new dag

---
+++ dags/minha_oitava_dag.py
@@ -0,0 +1,40 @@
+from time import sleep
+from airflow.decorators import dag, task
+from datetime import datetime
+
+@dag(
+    dag_id='minha_oitava_dag',
+    start_date=datetime(2021, 1, 1),
+    schedule='@daily',
+    catchup=False
+)
+def pipeline():
+
+    @task
+    def primeira_atividade():
+        print("Primeira atividade")
+        sleep(2)
+
+    @task
+    def segunda_atividade():
+        print("Segunda atividade")
+        sleep(2)
+
+    @task
+    def terceira_atividade():
+        print("Terceira atividade")
+        sleep(2)
+
+    @task
+    def quarta_atividade():
+        print("Pipeline finalizada")
+        sleep(2)
+
+    t1 = primeira_atividade()
+    t2 = segunda_atividade()
+    t3 = terceira_atividade()
+    t4 = quarta_atividade()
+
+    t1 >> t2 >> t3 >> t4
+
+pipeline()
